bec9372eb62237cb813e673fed866dcc5598ca5c
neilmillard-msm <neil.millard@moneysupermarket.com>
2016-12-29 14:18:55 +0000

    fix up runner and plugins (LF CRLF)

--- clean.sh
+++ clean.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # cleans docker images
-docker stop jenkins-nginx
-docker rm jenkins-nginx
-docker stop jenkins-master
-docker rm jenkins-master
-docker rm jenkins-data
+docker stop jenkins_nginx_1
+docker rm jenkins_nginx_1
+docker stop jenkins_master_1
+docker rm jenkins_master_1
+docker rm jenkins_data_1
 
 docker rmi $(docker images -q -f "dangling=true")
--- runner.sh
+++ runner.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-docker build -t myjenkins jenkins-master/.
-docker build -t myjenkinsdata jenkins-data/.
-docker build -t myjenkinsnginx jenkins-nginx/.
+#docker build -t myjenkins jenkins-master/.
+#docker build -t myjenkinsdata jenkins-data/.
+#docker build -t myjenkinsnginx jenkins-nginx/.
+#
+#docker run --name=jenkins-data myjenkinsdata
+#docker run -p 50000:50000 --name=jenkins-master --volumes-from=jenkins-data -d myjenkins
+#docker run -p 80:80 --name=jenkins-nginx --link jenkins-master:jenkins-master -d myjenkinsnginx
 
-docker run --name=jenkins-data myjenkinsdata
-docker run -p 50000:50000 --name=jenkins-master --volumes-from=jenkins-data -d myjenkins
-docker run -p 80:80 --name=jenkins-nginx --link jenkins-master:jenkins-master -d myjenkinsnginx
+docker-compose -p jenkins build
+docker rmi $(docker images -q -f "dangling=true")
+docker-compose -p jenkins up -d nginx data master
